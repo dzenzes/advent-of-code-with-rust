3537df98b9b4e0bdbcb98c449ddaf9f750db7aba
Daniel Zenzes <zenzes@bryter.io>
2022-12-01 08:55:24 +0100

    solution for day 1

---
+++ day01/src/main.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let path = "./input.txt";
+    let input = File::open(path).expect("couldn't open file");
+    let buffered = BufReader::new(input);
+
+    let mut calories = Vec::new();
+
+    let mut current_calories = 0;
+
+    for line in buffered.lines() {
+        if let Ok(ip) = line {
+            match ip {
+                ip if ip.is_empty() => {
+                    calories.push(current_calories);
+                    current_calories = 0
+                }
+                _ => {
+                    let num: u64 = ip.trim().parse().expect("Illegal input");
+                    current_calories += num
+                }
+            }
+        }
+    }
+    calories.sort();
+    let max_value: u64 = *calories.iter().max().expect("Couldn't find max value");
+
+    println!("Solution for day 01/1: {max_value}");
+
+    let max_three_values_sum: u64 = calories.iter().rev().take(3).sum();
+
+    println!("Solution for day 01/2: {max_three_values_sum}");
+}
