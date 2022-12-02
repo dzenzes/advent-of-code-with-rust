2ecd0e810cf001c342236c949f34ddf1e6f9329c
Daniel Zenzes <daniel@zenzes.me>
2022-12-02 18:55:32 +0100

    chore: added some tests

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,7 +1,7 @@
+use crate::GameOutcome::{DRAW, LOSE, WIN};
+use crate::Strategy::{PAPER, ROCK, SCISSOR};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use crate::GameOutcome::{DRAW, LOSE, WIN};
-use crate::Strategy::{PAPER, SCISSOR, ROCK};
 
 enum Strategy {
     SCISSOR,
@@ -15,20 +15,20 @@ impl Strategy {
             LOSE => match &self {
                 SCISSOR => PAPER,
                 PAPER => ROCK,
-                ROCK => SCISSOR
-            }
+                ROCK => SCISSOR,
+            },
 
             DRAW => match &self {
                 SCISSOR => SCISSOR,
                 PAPER => PAPER,
-                ROCK => ROCK
-            }
+                ROCK => ROCK,
+            },
 
             WIN => match &self {
                 SCISSOR => ROCK,
                 PAPER => SCISSOR,
-                ROCK => PAPER
-            }
+                ROCK => PAPER,
+            },
         }
     }
 }
@@ -49,11 +49,9 @@ struct DuelWithOutcome {
     outcome: GameOutcome,
 }
 
-
 const LOST_GAME_POINTS: u32 = 0;
 const DRAW_GAME_POINTS: u32 = 3;
 const WON_GAME_POINTS: u32 = 6;
-
 
 const ROCK_POINTS: u32 = 1;
 const PAPER_POINTS: u32 = 2;
@@ -65,34 +63,46 @@ impl Duel {
             "A" => ROCK,
             "B" => PAPER,
             "C" => SCISSOR,
-            _ => panic!("couldn't parse strategy")
+            _ => panic!("couldn't parse strategy"),
         };
         let own_strategy = match own_raw {
             "X" => ROCK,
             "Y" => PAPER,
             "Z" => SCISSOR,
-            _ => panic!("couldn't parse strategy")
+            _ => panic!("couldn't parse strategy"),
         };
 
-        Duel { elf_strategy, own_strategy }
+        Duel {
+            elf_strategy,
+            own_strategy,
+        }
     }
 
     fn play(&self) -> u32 {
         match &self.own_strategy {
-            SCISSOR => SCISSOR_POINTS + match &self.elf_strategy {
-                SCISSOR => DRAW_GAME_POINTS,
-                PAPER => WON_GAME_POINTS,
-                ROCK => LOST_GAME_POINTS
-            },
-            PAPER => PAPER_POINTS + match &self.elf_strategy {
-                SCISSOR => LOST_GAME_POINTS,
-                PAPER => DRAW_GAME_POINTS,
-                ROCK => WON_GAME_POINTS
-            },
-            ROCK => ROCK_POINTS + match &self.elf_strategy {
-                SCISSOR => WON_GAME_POINTS,
-                PAPER => LOST_GAME_POINTS,
-                ROCK => DRAW_GAME_POINTS
+            SCISSOR => {
+                SCISSOR_POINTS
+                    + match &self.elf_strategy {
+                        SCISSOR => DRAW_GAME_POINTS,
+                        PAPER => WON_GAME_POINTS,
+                        ROCK => LOST_GAME_POINTS,
+                    }
+            }
+            PAPER => {
+                PAPER_POINTS
+                    + match &self.elf_strategy {
+                        SCISSOR => LOST_GAME_POINTS,
+                        PAPER => DRAW_GAME_POINTS,
+                        ROCK => WON_GAME_POINTS,
+                    }
+            }
+            ROCK => {
+                ROCK_POINTS
+                    + match &self.elf_strategy {
+                        SCISSOR => WON_GAME_POINTS,
+                        PAPER => LOST_GAME_POINTS,
+                        ROCK => DRAW_GAME_POINTS,
+                    }
             }
         }
     }
@@ -104,16 +114,19 @@ impl DuelWithOutcome {
             "A" => ROCK,
             "B" => PAPER,
             "C" => SCISSOR,
-            _ => panic!("couldn't parse strategy")
+            _ => panic!("couldn't parse strategy"),
         };
         let outcome = match own_raw {
             "X" => LOSE,
             "Y" => DRAW,
             "Z" => WIN,
-            _ => panic!("couldn't parse strategy")
+            _ => panic!("couldn't parse strategy"),
         };
 
-        DuelWithOutcome { elf_strategy, outcome }
+        DuelWithOutcome {
+            elf_strategy,
+            outcome,
+        }
     }
 
     fn play(&self) -> u32 {
@@ -122,10 +135,13 @@ impl DuelWithOutcome {
         let elf_strategy = match &self.elf_strategy {
             SCISSOR => SCISSOR,
             PAPER => PAPER,
-            ROCK => ROCK
+            ROCK => ROCK,
         };
 
-        let duel = Duel { elf_strategy, own_strategy };
+        let duel = Duel {
+            elf_strategy,
+            own_strategy,
+        };
         duel.play()
     }
 }
@@ -138,7 +154,6 @@ fn main() {
     let mut duels: Vec<Duel> = Vec::new();
     let mut duels_with_outcome: Vec<DuelWithOutcome> = Vec::new();
 
-
     for line in buffered.lines() {
         if let Ok(ip) = line {
             let mut duel_raw = ip.split_whitespace();
@@ -147,7 +162,7 @@ fn main() {
             duels.push(Duel::create(&elf_raw, &own_raw));
             duels_with_outcome.push(DuelWithOutcome::create(&elf_raw, &own_raw));
         }
-    };
+    }
     let mut result: u32 = 0;
     for duel in duels {
         result += duel.play()
@@ -159,4 +174,28 @@ fn main() {
         result += duel.play()
     }
     println!("Result for ay 02/02: {result}");
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_duel_1() {
+        let duel = Duel::create("A", "Y");
+        assert_eq!(duel.play(), 8);
+    }
+
+    #[test]
+    fn test_duel_2() {
+        let duel = Duel::create("B", "X");
+        assert_eq!(duel.play(), 1);
+    }
+
+    #[test]
+    fn test_duel_3() {
+        let duel = Duel::create("C", "Z");
+        assert_eq!(duel.play(), 6);
+    }
 }
