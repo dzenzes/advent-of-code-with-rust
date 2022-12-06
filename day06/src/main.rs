8e2e89d5c22a1f9abe4942af44833963cc9a7767
Daniel Zenzes <daniel@zenzes.me>
2022-12-06 07:57:24 +0100

    feat: solution for day 6

---
+++ day06/src/main.rs
@@ -0,0 +1,88 @@
+use std::collections::HashSet;
+
+fn no_duplicates(input: &str) -> bool {
+    let char_vector: Vec<char> = input.chars().collect();
+    let unique_characters: HashSet<char> = HashSet::from_iter(char_vector);
+    let number_of_duplicates = input.len() - unique_characters.len();
+    return number_of_duplicates == 0;
+}
+
+fn first_marker_at(input: &str, marker_size: usize) -> usize {
+    for index in 0..input.len() {
+        if index >= marker_size {
+            let marker_candidate = &input[index - marker_size..index];
+            if no_duplicates(&marker_candidate) {
+                return index;
+            }
+        }
+    }
+    panic!("Couldn't find marker");
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let result = first_marker_at(input, 4);
+    println!("Result for day 06/01: {result}");
+    let result = first_marker_at(input, 14);
+    println!("Result for day 06/02: {result}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example_1() {
+        let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        assert_eq!(first_marker_at(input, 4), 5);
+    }
+
+    #[test]
+    fn test_example_2() {
+        let input = "nppdvjthqldpwncqszvftbrmjlhg";
+        assert_eq!(first_marker_at(input, 4), 6);
+    }
+
+    #[test]
+    fn test_example_3() {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        assert_eq!(first_marker_at(input, 4), 10);
+    }
+
+    #[test]
+    fn test_example_4() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        assert_eq!(first_marker_at(input, 4), 11);
+    }
+
+    // part 2
+    #[test]
+    fn test_example_2_1() {
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        assert_eq!(first_marker_at(input, 14), 19);
+    }
+
+    #[test]
+    fn test_example_2_2() {
+        let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        assert_eq!(first_marker_at(input, 14), 23);
+    }
+
+    #[test]
+    fn test_example_2_3() {
+        let input = "nppdvjthqldpwncqszvftbrmjlhg";
+        assert_eq!(first_marker_at(input, 14), 23);
+    }
+
+    #[test]
+    fn test_example_2_4() {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        assert_eq!(first_marker_at(input, 14), 29);
+    }
+
+    #[test]
+    fn test_example_2_5() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        assert_eq!(first_marker_at(input, 14), 26);
+    }
+}
